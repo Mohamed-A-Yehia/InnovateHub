211f1caa985d5b3711d94807ab251d097fd4eef0
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-07-12 05:08:56 +0300

    Setup The Project Structure & Starter Files

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-[100vh] items-center justify-center bg-sky-50">
+      <h1 className="text-9xl font-bold text-blue-500 italic">InnovateHub</h1>
+    </div>
+  );
+}
+
+export default App;
