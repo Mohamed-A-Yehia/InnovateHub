133b6c99031bd266e12156a8ee6548f3158f4087
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-07-14 02:43:40 +0300

    Re-direct files to the right place & making a simple header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
+import HeaderCTA from "./HeaderCTA";
 import Logo from "./Logo";
 import Nav from "./Nav";
 
 function Header() {
   return (
-    <header className="container m-auto flex items-center justify-between py-9">
+    <header className="m-auto flex items-center justify-between bg-black px-11 py-4 text-amber-50">
       <Logo />
       <Nav />
+      <HeaderCTA />
     </header>
   );
 }
