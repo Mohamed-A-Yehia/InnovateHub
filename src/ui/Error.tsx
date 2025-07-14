3a8dfc50a1a45ccf59284c1adc4a62ac2f72d7a2
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-07-15 01:14:35 +0300

    simple Link Button

--- src/ui/Error.tsx
+++ src/ui/Error.tsx
@@ -1,12 +1,15 @@
 import { useRouteError } from "react-router";
+import LinkButton from "./LinkButton";
 
 function Error() {
   const error = useRouteError() as Error;
 
   return (
-    <div className="flex h-96 items-center justify-center">
+    <div className="flex h-96 flex-col items-center justify-center gap-1.5">
       <h1 className="text-3xl font-bold">Something Went Wrong</h1>
       <p>{error.message}</p>
+
+      <LinkButton />
     </div>
   );
 }
---
+++ src/ui/LinkButton.tsx
@@ -0,0 +1,5 @@
+function LinkButton() {
+  return <button className="cursor-pointer">Go Back</button>;
+}
+
+export default LinkButton;
